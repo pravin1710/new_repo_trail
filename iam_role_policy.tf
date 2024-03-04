c6ed7082edaf55b39f580ac9e9c149e864dea91f
pravin1710 <pravinsangve17@gmail.com>
2024-03-04 19:00:52 +0530

    commit2

--- iam_role_policy.tf
+++ iam_role_policy.tf
@@ -20,5 +20,5 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::238573859336:role/ecsInstanceRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
